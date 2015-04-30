998e45324543fe39a23958e8a6ea14c0d8b7c0fa
lau1406 <lkeijzer@live.nl>
2015-04-30 15:13:14 +0200

    rotation almost done

--- src/Player.java
+++ src/Player.java
@@ -18,6 +18,7 @@ public class Player {
 	int moveX, moveY;
 	int health;
 	int speedLimit = 5;
+	double rotation;
 
 	double temp;
 
@@ -37,17 +38,76 @@ public class Player {
 		BufferedImage imagePlayer;
 
 		temp += 0.05;
+		rotation = calcRotation();
 
 		try{
 			imagePlayer = ImageIO.read(new File("images//square1.png"));
-			//g.translate(-20, -20);
+			g.translate(-20, -20);
 			AffineTransform tx = new AffineTransform();
-			tx.rotate(temp, imagePlayer.getWidth() / 2, imagePlayer.getHeight() / 2);
+			tx.rotate(rotation, imagePlayer.getWidth() / 2, imagePlayer.getHeight() / 2);
 			AffineTransformOp op = new AffineTransformOp(tx,AffineTransformOp.TYPE_BILINEAR);
 			imagePlayer = op.filter(imagePlayer, null);
 			g.drawImage(imagePlayer, x, y, null);
 		} catch( IOException e ) {
 			//e.printStackTrace();
+		}
+	}
+
+	double calcRotation(){
+		double deltaX, deltaY;
+		double tempAngle;
+		deltaX = mainScript.mouseLoc.x - x;
+		deltaY = mainScript.mouseLoc.y - y;
+
+		//System.out.println(deltaX);
+		//System.out.println(deltaY);
+
+		if (deltaX <= 0 && deltaY <= 0 ){
+			System.out.println("1");
+			try{
+				tempAngle = Math.atan(deltaX / deltaY);
+				System.out.println(tempAngle);
+				return (tempAngle * -1);
+			}catch(Exception e){
+				System.out.println("exception");
+				return (Math.PI * 0.5);
+			}
+		}
+
+		else if (deltaX >= 0 && deltaY <= 0 ){
+			System.out.println("2");
+			try{
+				tempAngle = Math.atan(deltaX / deltaY);
+				return (tempAngle * -1);
+			}catch(Exception e){
+				System.out.println("exception");
+				return (Math.PI * -0.5);
+			}
+		}
+
+		else if (deltaX <= 0 && deltaY >= 0 ){
+			System.out.println("3");
+			try{
+				tempAngle = Math.atan(deltaX / deltaY);
+				return (Math.PI - tempAngle);
+			}catch(Exception e){
+				System.out.println("exception");
+				return (Math.PI * 0.5);
+			}
+		}
+
+		else if (deltaX >= 0 && deltaY >= 0 ){
+			System.out.println("4");
+			try{
+				tempAngle = Math.atan(deltaX / deltaY);
+				return (Math.PI - tempAngle);
+			}catch(Exception e){
+				System.out.println("exception");
+				return (Math.PI * -0.5);
+			}
+		}
+		else {
+			return 0;
 		}
 	}
 }
